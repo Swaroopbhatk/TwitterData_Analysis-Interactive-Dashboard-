de4fe8ee355597827c36c8b4cdd779a5051d9b0a
Swaroop S Bhat <swaroopbhatk@gmail.com>
2018-06-23 23:25:29 +0100

    Update DVAssignment3_TwitterDataExtraction_17230755.R

--- DVAssignment3_TwitterDataExtraction_17230755.R
+++ DVAssignment3_TwitterDataExtraction_17230755.R
@@ -3,10 +3,10 @@ library(twitteR)
 library(base64enc)
 
 # Change the next four lines based on your own consumer_key, consume_secret, access_token, and access_secret. 
-consumer_key <- "0Vfu5oSPVU0TdWZPvQYqMbmLN"
-consumer_secret <- "jTW4evnQb2FDr0nL65saiPFbos5j2y41ZY8SdMraVJONjF85yb"
-access_token <- "973186672409108480-HjveME25yvLyWSi8iCykN9XJ78ERC39"
-access_secret <- "eikbWqXsQzupbPyJLZoxidO3EFnRkWzjUNtDnH9xmw1s8"
+consumer_key <- "YourKey"
+consumer_secret <- "YourKey"
+access_token <- "YourKey"
+access_secret <- "YourKey"
 
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 tw = twitteR::searchTwitter('#IrishWater', n = 700, since = '2017-01-01', retryOnRateLimit = 1e3)
